b8850f0dce51c36d3092d55109096251d6e8761c
SUJAY GOWDA <sujay.gowda@i4sight.net>
2023-10-29 19:21:09 +0530

    Vite + React Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 className=' text-cyan-500 text-6xl flex justify-center '>
+      Hellow World
+    </h1>
+  );
+}
+
+export default App;
